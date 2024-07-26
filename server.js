49a6c1b8175d2097b4c276093d7f0169eb0227a7
Haiaza <83097184+Haiaza@users.noreply.github.com>
2024-07-26 19:51:38 +0000

    lines 11-14 are related to the program error

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+//* It matters this is at the VERY top so the variables are loaded globally
+const dotenv = require('dotenv')
+dotenv.config() // loads the enviornment variables
 // pull the server requirements
 const express = require('express');
+const mongoose = require('mongoose')
 
 //initialize the server
 const app = express();
 
-
-
+mongoose.connect(process.env.MONGODB_URI); //our tool to connect to MongoDB using my URI inside of .env
+mongoose.connection.on("connected", () =>{
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
 
 app.listen(3000, () =>{
     console.log('Port 3000 is active')
