61da31f58258f74c500af328276b1a27b04b4729
Haiaza <83097184+Haiaza@users.noreply.github.com>
2024-07-26 17:55:50 +0000

    Server has been n initialized

---
+++ server.js
@@ -0,0 +1,12 @@
+// pull the server requirements
+const express = require('express');
+
+//initialize the server
+const app = express();
+
+
+
+
+app.listen(3000, () =>{
+    console.log('Port 3000 is active')
+})
