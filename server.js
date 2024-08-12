bfa9788b78c78e5438c32928a8245048a7d6d335
haiaza <ajames1342@gmail.com>
2024-08-12 03:38:08 -0400

    Delete func

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ dotenv.config() // loads the enviornment variables
 // pull the server requirements
 const express = require('express');
 const mongoose = require('mongoose')
+const methodOverride = require("method-override");
+const morgan = require("morgan")
 const MONGODB_URI = process.env.MONGODB_URI
 //initialize the server
 const app = express();
@@ -12,6 +14,8 @@ const app = express();
 const Fruit = require('./models/fruit')
 
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); 
+app.use(morgan("dev")); 
 
 
 mongoose.connect(MONGODB_URI); //our tool to connect to MongoDB using my URI inside of .env
@@ -38,6 +42,11 @@ app.get('/fruits/new', (req, res) =>{
     res.render('fruits/new.ejs')
 })
 
+app.get("/fruits/:fruitId", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", { fruit: foundFruit });
+
+});
 app.post('/fruits', async (req, res) => {
     if (req.body.isReadyToEat === "on") {
         req.body.isReadyToEat = true;
@@ -47,3 +56,7 @@ app.post('/fruits', async (req, res) => {
     await Fruit.create(req.body);
     res.redirect("/fruits/");
 })
+app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+});
